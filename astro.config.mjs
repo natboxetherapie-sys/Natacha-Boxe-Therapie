431992cdfc540f7b1bc28ed80bab780a8a49289d
Natacha_BT <nat.boxetherapie@gmail.com>
2026-05-11 18:14:41 +0200

    config deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://natboxetherapie-sys.github.io',
+  base: '/Natacha-Boxe-Therapie',
+});
